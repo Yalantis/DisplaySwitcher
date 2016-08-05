9671891cd13f8606d3863d3f5f42d1651478fde4
Serhii Butenko <sereja.butenko@gmail.com>
2016-08-05 08:23:27 +0300

    Update example for cocoa pods 1.0.1

--- Example/Pods/Target Support Files/Pods-DisplaySwitcher_Tests/Pods-DisplaySwitcher_Tests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DisplaySwitcher_Tests : NSObject
-@end
-@implementation PodsDummy_Pods_DisplaySwitcher_Tests
-@end
--- Example/Pods/Target Support Files/Pods-DisplaySwitcher_Tests/Pods-DisplaySwitcher_Tests-umbrella.h
+++
@@ -1,6 +0,0 @@
-#import <UIKit/UIKit.h>
-
-
-FOUNDATION_EXPORT double Pods_DisplaySwitcher_TestsVersionNumber;
-FOUNDATION_EXPORT const unsigned char Pods_DisplaySwitcher_TestsVersionString[];
-
